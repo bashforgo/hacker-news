c30663044a4d1a7eb7b043acbfa733d7ed5d379e
Barnabas Forgo <Barnabas.Forgo-CIC-UK@ibm.com>
2018-12-27 10:49:15 +0000

    fix router base url

--- src/root/Root.tsx
+++ src/root/Root.tsx
@@ -1,5 +1,5 @@
 import React, { Component, ReactNode } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import LanguageProvider from './LanguageProvider/LanguageProvider'
 import StorageProvider from './StorageProvider/StorageProvider'
 import ThemeProvider from './ThemeProvider/ThemeProvider'
@@ -8,11 +8,11 @@ class Root extends Component {
   public render(): ReactNode {
     return (
       <StorageProvider>
-        <BrowserRouter>
+        <HashRouter basename={process.env.PUBLIC_URL}>
           <ThemeProvider>
             <LanguageProvider>{this.props.children}</LanguageProvider>
           </ThemeProvider>
-        </BrowserRouter>
+        </HashRouter>
       </StorageProvider>
     )
   }
